4177445b60d4e4496edc6658c21760b64ac82620
Muneeb Shahid <muneeb706@gmail.com>
2021-12-05 15:36:45 -0800

    actions and forms integrated

--- react-graphql-client/src/App.js
+++ react-graphql-client/src/App.js
@@ -1,12 +1,67 @@
-import './App.css';
-import CustomNavbar from './custom-navbar/CustomNavbar';
+import { useMemo, useState } from 'react'
+import './App.css'
+import CustomNavbar from './custom-navbar/CustomNavbar'
+import Add from './actions/Add'
+import { ACTION } from './utils/constant'
+import Search from './actions/Search'
 
-function App() {
+const App = () => {
+  const [selectedAction, setSelectedAction] = useState(ACTION.clinic.search)
+
+  const updateAction = (action) => {
+    setSelectedAction(action)
+  }
+
+
+  const getEntityNameByAction = (action) => {
+    let selectedEntities = Object.keys(ACTION).filter(
+      (entity) => ACTION[entity][action] === selectedAction,
+    )
+    return selectedEntities ? selectedEntities[0] : null
+  }
+
+  const isAddAction = () => {
+    const entity = getEntityNameByAction('add')
+    return entity ? true : false
+  }
+
+  const isSearchAction = () => {
+    const entity = getEntityNameByAction('search')
+    return entity ? true : false
+  }
+
+  const selectedEntity = useMemo(()=>{
+    return selectedAction.split('-')[1]
+  }, [selectedAction])
+
+  const links = [
+    {
+      id: 'clinic',
+      title: 'Clinic',
+      onAdd: () => updateAction(ACTION.clinic.add),
+      onSearch: () => updateAction(ACTION.clinic.search),
+    },
+    {
+      id: 'patient',
+      title: 'Patient',
+      onAdd: () => updateAction(ACTION.patient.add),
+      onSearch: () => updateAction(ACTION.patient.search),
+    },
+    {
+      id: 'visit',
+      title: 'Visit',
+      onAdd: () => updateAction(ACTION.visit.add),
+      onSearch: () => updateAction(ACTION.visit.search),
+    },
+  ]
+
   return (
     <div className="App">
-      <CustomNavbar />
+      <CustomNavbar links={links} />
+      {isAddAction() && <Add entity={selectedEntity} />}
+      {isSearchAction() && <Search entity={selectedEntity}/>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
